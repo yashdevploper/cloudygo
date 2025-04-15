b896eca4d4bbf57d3d6c5d8ba30d8cc691b88f60
yashdevploper <yashbhuva.programmer@gmail.com>
2025-04-15 23:20:37 +0530

    Update ThemeToggle.tsx

--- components/appComponents/ThemeToggle.tsx
+++ components/appComponents/ThemeToggle.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { useTheme } from "@/context/themeProvider";
 import { Avatar, AvatarImage, AvatarFallback } from "../ui/avatar";
 
-
 const ThemeToggle = () => {
   const { theme, setTheme } = useTheme();
 
@@ -14,14 +13,12 @@ const ThemeToggle = () => {
       {theme === "Dark" ? (
         <Avatar className="w-[24px] hover:rotate-90  transition duration-200">
           <AvatarImage src="/lightMod.svg" />
-          <AvatarFallback>
-            theme toggle
-          </AvatarFallback>
+          <AvatarFallback>theme toggle</AvatarFallback>
         </Avatar>
       ) : (
         <Avatar className="w-[24px] rotate-12 hover:-rotate-z-[20deg]  transition duration-200">
           <AvatarImage src="/darkMod.svg" />
-          <AvatarFallback>Profile</AvatarFallback>
+          <AvatarFallback></AvatarFallback>
         </Avatar>
       )}
     </button>
